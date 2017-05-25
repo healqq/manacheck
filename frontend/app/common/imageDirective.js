2e0ab39e4f09fb53b96b8f8cb1fe12634698cd5e
healqq <nyashin92@gmail.com>
2017-05-25 20:21:51 +0300

    added gzip, fixed style, fixed unused images loading

--- frontend/app/common/imageDirective.js
+++ frontend/app/common/imageDirective.js
@@ -5,7 +5,31 @@
         .module('common')
         .directive('mcImage', imageDirective);
 
-    function imageDirective() {
+    imageDirective.$inject = ['$window', '$document'];
+    function imageDirective($window, $document) {
+        var DISPLAY_NONE = 'none';
+        function linkFunction($scope, element) {
+            if (isVisible()) {
+                loadImage($scope.url);
+            } 
+
+            function isVisible() {
+                return $window.getComputedStyle(element[0]).display !== DISPLAY_NONE;
+            }
+
+            function loadImage(link) {
+                var img = angular.element('<img/>')[0];
+                img.src = link;
+                angular.element(img).on('load', function () {
+                    var $element = angular.element(element);
+                    $element
+                        .css('background-image', 'url(' + link + ')')
+                        .css('opacity', 1);
+                });
+                img = null;
+            }
+
+        }
         return {
             restrict: 'A',
             scope: {
@@ -16,21 +40,6 @@
             },
         };
     }
-
-    function linkFunction($scope, element) {
-        loadImage($scope.url);
-
-        function loadImage(link) {
-            var img = angular.element('<img/>')[0];
-            img.src = link;
-            angular.element(img).on('load', function () {
-                var $element = angular.element(element);
-                $element
-                    .css('background-image', 'url(' + link + ')')
-                    .css('opacity', 1);
-            });
-        }
-
-    }
+    
 
 })();
